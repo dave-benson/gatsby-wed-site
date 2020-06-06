449f4e3f0ca5ad680b59e58ad37cd5a4dee86715
davidrossbenson85 <davidrossbenson85@googlemail.com>
2020-06-06 15:27:14 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Sarah & Dave"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
